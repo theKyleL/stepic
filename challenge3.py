3db61251f6ba28b926d341c7c99414857a8cbb30
Kyle Latino <kyle.latino@gmail.com>
2017-07-18 01:24:04 -0400

    added basic structure to challenge3.py
    Needs implementation and testing

--- challenge3.py
+++ challenge3.py
@@ -4,3 +4,21 @@ __author__ = 'Kyle Latino'
 #
 #
 #
+
+import struct
+# accept input from std in identifying file name
+inputFile = input()
+
+with open(inputFile, ‘rb’) as img:
+
+
+# repeat till end of file
+#while :
+
+# scan file for bitmap header (BM / 0x42 0x4D)
+# mark header offset for file_name
+
+
+# scan file for tail
+# write data to file 
+
