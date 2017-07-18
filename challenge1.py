73ccc2b1a4f1fed4da18640be6eb95a04e37a234
Kyle <kyle.latino@gmail.com>
2017-07-17 23:37:32 -0400

    challenge 1 and 2 completed
    challenge1 can be simplified using import struct... but may want to leave it out to illustrate python stuff..

--- challenge1.py
+++ challenge1.py
@@ -1,14 +1,19 @@
 # challenge 1
+# written in python 3
 __author__ = 'Kyle Latino'
 # Blum Blum Shub Pseudo Random Number Generator
-# 
+#
 # Value = (Value^2) % M
 # M = 0xE2089EA5
-# 
+#
 
 import sys
 
-value = 0x12345678 # initial value will be passed in from function signature
+if(sys.version_info[0] < 3):
+	print('\n\nchallenge1.py requires Python version 3 or greater.\nUse command\'python3 challenge1.py\'\n\n')
+	sys.exit()
+
+value = 0xDEADBEEF # initial value will be passed in from function signature
 M = 0xE2089EA5 # modulous remains constant in this implementation
 
 # determine the next value for the key stream
@@ -26,7 +31,7 @@ def xor(character, keyValue, isHex):
 	if isHex:
 		return int(int(('0x' + character[2:]), 16) ^ int(keyValue, 16))
 	return int(ord(character)) ^ int(keyValue, 16)
-			
+
 # format value to match requirements
 # e.g. Use '\x##' instead of '0x##' for input and output of hex values
 def alterInputHex(inputHexValue):
@@ -35,51 +40,59 @@ def alterInputHex(inputHexValue):
 
 def alterOutputHex(outputHexValue):
 	return outputHexValue.replace('0x', '\\x')
-	
+
 def printOut(data):
 	dat = ''.join(data)
 	dat = alterOutputHex(dat)
 	print(dat)
-	
-# fit this function signature!!		
+
+# fit this function signature!!
 def Crypt(unsignedChar, dataLength, initVal):
 	global value
 	print((unsignedChar))
 	dat = [] # output data stored here
-	inputChr = (unsignedChar) # 
+	inputChr = (unsignedChar) #
 	value = (initVal) # initialize value from input
 	isHex = False
-	
+
 	# check for hex input
-	if inputChr[:2] == r'\x': # compare the 'raw' value of the string, ignoring ‘escape’ characters
+	if inputChr[:2] == r'\x':
 		isHex = True
 		inputValue = alterInputHex(inputChr) # convert input hex value to python native format
 		#process hex
 		for i in range(dataLength):
 			nextValue()
 			dat.append(chr(xor(unsignedChar[(4*i):(4*i)+4], extractKey(), isHex)))
-	
+
 	else:
 		# process chars
 		for i in range(dataLength):
 			nextValue()
 			dat.append(str(hex(xor(unsignedChar[i], extractKey(), isHex))))
-	
+
 	printOut(dat)
-	
-	
+
+
 if __name__ == '__main__':
-	
+
 	#print(nextValue(0x10000)) # verify proper implementation of func output. 0x10000 => 0x1DF7615B
 
 	test1 = Crypt('apple', 5, 0x12345678)
-	
+
 	print() # separate tests for readability
-	
-	test2 = Crypt(r'\x4c\x88\x9e\xdf\xe8', 5, 0x12345678)	
+
+	test2 = Crypt(r'\x4c\x88\x9e\xdf\xe8', 5, 0x12345678)
 	#
 	# NOTE: the use of the 'r' preceeding the input string ensures that the value is passed in its raw format.
 	#
-	# Without the 'r' it would be necessary to pass an escaped version of the string containing the hex value. 
+	# Without the 'r' it would be necessary to pass an escaped version of the string containing the hex value.
 	# (e.g. '\\x4c\\x88\\x9e\\xdf\\xe8')
 	#
+
+	print() # separate tests for readability
+
+	test3 = Crypt(r'bannana', 7, 0x12345678)
+
+	print() # separate tests for readability
+
+	test2 = Crypt(r'\x4f\x99\x80\xdd\xec\xd7\x7e', 7, 0x12345678)
--- challenge2.py
+++ challenge2.py
@@ -1,30 +1,37 @@
 # challenge 2
+# written in python 3
 __author__ = 'Kyle Latino'
-# Bitmap image tool will accept input path to .bmp and 
+# Bitmap image tool will accept input path to .bmp and
 # generate a file with the height and width dimensions in pixels.
-# 
-# 
-# height data found in byte23
-# width data found in byte19
+#
+# .bmp data locations, 2 bytes, little endian
+# height data found in bytes 22&23
+# width data found in bytes 18&19
 
+import struct
 
-img = input('Enter path to image:' + '\n') # store path to *.bmp image as string
+if(sys.version_info[0] < 3):
+	print('\n\nchallenge1.py will run under python 2.7 but the image address must be contained within quotes.\n Recommend running in python 3.\nUse command\'python3 challenge2.py\'\n\n')
 
+image = input('Enter path to image:' + '\n') # store path to *.bmp image as string
 output = 'imageDataOutput.txt'
-print('Image address: ' + img)
+print('Image address: ' + image)
 
 try:
 	with open(image, 'rb') as img:
-		height = img.seek(22)
-		width = img.seek(18)
-		
-		with open(output, 'w+') as out:
-			out.write('Image height: ' + height + '\n')
-			out.write('Image width: ' + width + '\n')
-			
-		print('height: ' + height + '\nwidth: ' + width)
-		
-except :
-	print('Unable to open image at location: ' + img)
-	
+		img.seek(18)
+		width = struct.unpack('i', img.read(4))
+		widthString = ('width: ' + str(width[0]))
 
+		img.seek(22)
+		height = struct.unpack('i', img.read(4))
+		heightString = ('height: ' + str(height[0]))
+
+
+		with open(output, 'w+') as out:
+			out.write(heightString + '\n')
+			out.write(widthString + '\n')
+
+			print(heightString + '\n' + widthString)
+except:
+	print('Unable to open image at location: ' + img)
